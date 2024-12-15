ecd7885273d54c3d3ed39864b831ff9c5189cfc1
Shahriyar001 <shahriyarmahbub55@gmail.com>
2024-12-15 23:44:45 +0600

    added new componant for searchbar and catagori

---
+++ components/SearchBar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SearchBar = () => {
+  return (
+    <form className="searchbar" onSubmit={handleSearch}>
+      <div className="searchbar__item"></div>
+    </form>
+  );
+};
+
+export default SearchBar;
