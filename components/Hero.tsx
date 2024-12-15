f752f623f63b87a24295a62ad026ab2223e07877
Shahriyar001 <shahriyarmahbub55@gmail.com>
2024-12-15 15:53:46 +0600

    set layout ans added navbar

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,13 +1,13 @@
 "use client";
 import image from "next/image";
-import CustomButton from "./CustomButton";
 import Image from "next/image";
+import CustomButton from "./CustomButton";
 const Hero = () => {
   const handleScroll = () => {};
 
   return (
     <div className="hero">
-      <div className="flex-1 pt-36 padding-x">
+      <div className="flex-1 pt-36 padding-x ">
         <h1 className="hero__title">
           Find, book, or rent a car -- quickly and easily!
         </h1>
@@ -24,7 +24,7 @@ const Hero = () => {
       </div>
       <div className="hero__image-container">
         <div className="hero__image">
-          <Image src="/hero.png" alt="hero" fill className="object-contain" />
+          <Image src="/hero.png" alt="hero" fill className="object-contain " />
           <div className="hero__image-overlay"></div>
         </div>
       </div>
--- types/index.ts
+++ types/index.ts
@@ -4,4 +4,5 @@ export interface CustomButtonProps {
   title: string;
   containerStyles?: string;
   handeClick?: MouseEventHandler<HTMLButtonElement>;
+  btnType?: "button" | "submit";
 }
