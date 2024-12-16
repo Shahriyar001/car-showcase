3212c61b0ba4d2be2001218596dea0832979e299
Shahriyar001 <shahriyarmahbub55@gmail.com>
2024-12-16 23:58:57 +0600

    get cars

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import { CustomFilter, Hero, SearchBar } from "@/components";
+import { fetchCars } from "@/utils";
 
-export default function Home() {
+export default async function Home() {
+  const allCars = await fetchCars();
   return (
     <main className="overflow-hidden">
       <Hero />
