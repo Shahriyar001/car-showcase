1e5268bff03124f967707f83a5ebf68cdca64f40
Shahriyar001 <shahriyarmahbub55@gmail.com>
2024-12-19 23:56:09 +0600

    get side picture

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -54,7 +54,7 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
                   <div className="flex-1 flex flex-col gap-3">
                     <div className="relative w-full h-40 bg-pattern bg-cover bg-center rounded-lg">
                       <Image
-                        src={generateCarImageUrl(car, "angle")}
+                        src={generateCarImageUrl(car)}
                         alt="car modal"
                         fill
                         priority
@@ -64,7 +64,7 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
                     <div className="flex gap-3">
                       <div className="flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg">
                         <Image
-                          src={generateCarImageUrl(car, "angle")}
+                          src={generateCarImageUrl(car, "29")}
                           alt="car modal"
                           fill
                           priority
@@ -73,7 +73,7 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
                       </div>
                       <div className="flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg">
                         <Image
-                          src={generateCarImageUrl(car, "angle")}
+                          src={generateCarImageUrl(car, "33")}
                           alt="car modal"
                           fill
                           priority
@@ -82,7 +82,7 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
                       </div>
                       <div className="flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg">
                         <Image
-                          src={generateCarImageUrl(car, "angle")}
+                          src={generateCarImageUrl(car, "13")}
                           alt="car modal"
                           fill
                           priority
