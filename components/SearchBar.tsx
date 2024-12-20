2f1df9b264a1634befc746efeb177d0ff400bffc
Shahriyar001 <shahriyarmahbub55@gmail.com>
2024-12-20 23:51:41 +0600

    added button

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,6 +1,11 @@
 "use client";
 import { useState } from "react";
 import { SearchMenufacturer } from "./";
+import Image from "next/image";
+
+const SearchButton = ({ otherClasses }: { otherClasses: string }) => {
+  <button type="submit" className={`-ml-3 z-10 ${otherClasses}`}></button>;
+};
 
 const SearchBar = () => {
   const [menufacturer, setMenufacturer] = useState("");
@@ -11,6 +16,15 @@ const SearchBar = () => {
           menufacturer={menufacturer}
           setMenufacturer={setMenufacturer}
         />
+        <SearchButton otherClasses="sm:hidden">
+          <Image
+            src="/magnifying-glass.svg"
+            alt="magnifying glass"
+            width={40}
+            height={40}
+            className="object-contain"
+          />
+        </SearchButton>
       </div>
     </form>
   );
