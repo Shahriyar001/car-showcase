e06014d5c59896ff9a54fb1b11b83733904183c2
Shahriyar001 <shahriyarmahbub55@gmail.com>
2025-01-03 12:41:00 +0600

    load data to db and get in service page

--- components/RepairPage/Services.tsx
+++ components/RepairPage/Services.tsx
@@ -1,10 +1,17 @@
-import { services } from "@/lib/services";
 import React from "react";
 import ServiceCard from "@/components/cards/ServiceCard";
 
+const getServices = async () => {
+  const res = await fetch("http://localhost:3000/services/api/get-all");
+  const services = res.json();
+  return services;
+};
+
 const Services = async () => {
+  const { services } = await getServices();
+
   return (
-    <div className="text-slate-800 mb-24 w-full bg-slate-500">
+    <div className="text-slate-800 mb-24 w-full">
       <div className="text-center container mx-auto ">
         <h3 className="text-2xl font-bold text-orange-600">Our Services</h3>
         <h2 className="text-5xl">Our Service Area</h2>
@@ -16,9 +23,10 @@ const Services = async () => {
       </div>
       <div className="mx-auto md:ml-10 ml-20">
         <div className="container mx-auto mt-12 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-          {services.map((service) => (
-            <ServiceCard service={service} key={service._id} />
-          ))}
+          {services?.length > 0 &&
+            services?.map((service: any) => (
+              <ServiceCard service={service} key={service._id} />
+            ))}
         </div>
       </div>
     </div>
--- components/cards/ServiceCard.tsx
+++ components/cards/ServiceCard.tsx
@@ -15,7 +15,7 @@ const ServiceCard = ({ service }: ServiceCardProps) => {
         <div className="card-actions justify-between items-center">
           <h6 className="text-primary font-semibold">Price : ${price}</h6>
           <Link href={`/services/${_id}`}>
-            <button className="btn btn-primary">View Details</button>
+            <button className="btn btn-primary text-white">View Details</button>
           </Link>
         </div>
       </div>
