133f02b83b8200baf4075d7037fad98bee2da990
Shahriyar001 <shahriyarmahbub55@gmail.com>
2025-01-07 23:35:20 +0600

    added middleware

---
+++ middleware.ts
@@ -0,0 +1,20 @@
+import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
+
+export const middleware = async (request: Request) => {
+  const token = (await cookies(request)).get("next-auth.session-token");
+  const pathname = request.nextUrl.pathname;
+  if (pathname.includes("api")) {
+    return NextResponse.next();
+  }
+  if (!token) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+  return NextResponse.next();
+};
+
+export const config = {
+  matcher: ["/my-bookings/:path*", "/services/:path"],
+};
+
+// 9.06
